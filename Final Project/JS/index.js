f414e1d499a75972220d882e391f596302e7f3ff
kchutch <68035730+kchutch@users.noreply.github.com>
2020-09-09 21:24:04 -0500

    Add files via upload

---
+++ Final Project/JS/index.js
@@ -0,0 +1,11 @@
+//$("#slideshowleft > div:gt(0)").hide();
+
+/*setInterval(function () {
+  $("#slideshowleft > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo("#slideshowleft");
+}, 3000);
+*/
